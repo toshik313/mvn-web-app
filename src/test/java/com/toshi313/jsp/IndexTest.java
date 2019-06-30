e3b671d73f54f840bbb738c1e4c649eb29c59851
Toshiyuki Kobayashi <toshi.k.313+github@gmail.com>
2019-06-30 15:48:45 +0900

    seleniumでスクリーンショット

--- src/test/java/com/toshi313/jsp/IndexTest.java
+++ src/test/java/com/toshi313/jsp/IndexTest.java
@@ -1,20 +1,31 @@
 package com.toshi313.jsp;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.toshi313.common.Util;
 
 
 
 public class IndexTest {
 
+
     @Test
-    public void index_jspが表示されること() {
+    public void index_jspが表示されること() throws Exception {
 
         System.setProperty("webdriver.chrome.driver", "C:/Users/Toshiyuki/maven_prj/mvn-web-app/src/test/resources/chromedriver_win32/chromedriver.exe");
 
         WebDriver wd = new ChromeDriver();
         wd.get("http://localhost:8080/mvn-web-app/");
+
+        File image_temp = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(image_temp, new File("C:/Users/Toshiyuki/maven_prj/mvn-web-app/src/test/evidence/" + Util.getMethodName() + ".png"));
         wd.quit();
     }
 }
