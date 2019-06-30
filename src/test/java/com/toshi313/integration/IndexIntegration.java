e452989980c5b45091ffa2def623c79893df5cef
Toshiyuki Kobayashi <toshi.k.313+github@gmail.com>
2019-06-30 18:05:25 +0900

    jetty+failsafeplugin挑戦中

---
+++ src/test/java/com/toshi313/integration/IndexIntegration.java
@@ -0,0 +1,30 @@
+package com.toshi313.integration;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.toshi313.common.Util;
+
+public class IndexIntegration {
+
+    @Test
+    public void index_jspが表示されること() throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Toshiyuki/maven_prj/mvn-web-app/src/test/resources/chromedriver_win32/chromedriver.exe");
+
+        WebDriver wd = new ChromeDriver();
+//        wd.get("http://localhost:8080/mvn-web-app/");
+
+        String url = System.getProperty("url");
+        wd.get(url);
+        File image_temp = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(image_temp, new File("C:/Users/Toshiyuki/maven_prj/mvn-web-app/src/test/evidence/" + Util.getMethodName() + ".png"));
+        wd.quit();
+    }
+}
